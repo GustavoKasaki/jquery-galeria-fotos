a2d98dfca3f06ddf5c6fc2341648d4ccb2916f2c
GustavoKasaki <gmkasaki@gmail.com>
2024-07-17 21:16:11 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() { /* executa as funções quando o documento estiver pronto */
+    $('header button').click(function() { /* expande o menu ao clicar no botão 'nova imagem' */
+        $('form').slideDown();
+    })
+
+    $('#botaoCancelar').click(function() { /* retrai o menu ao clicar no botão 'cancelar' */
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) { /* cancela o efeito padrão de enviar ao clicar no botão 'enviar' */
+        e.preventDefault();
+        const endNovaImagem = $('#endImgNova').val(); /* .val = value no jQuery (se trata de uma função) */
+        const novoItem = $('<li style="display:none"></li>');
+        $(`<img src="${endNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlayImgLink">
+                <a href="${endNovaImagem}" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul'); /* insere a imagem na lista */
+        $(novoItem).fadeIn(1000); /* adiciona o efeito de transição à função de adicionar a imagem (1000 é o tempo de transição em ms */
+        $('#endImgNova').val(''); /* limpa o formulario */
+
+    })
+
+
+
+})
